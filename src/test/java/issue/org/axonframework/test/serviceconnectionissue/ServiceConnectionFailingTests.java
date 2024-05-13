1b04dad93fe71c57c78b43a70bddb21ba3b95be9
olivier.leborne <olivier.leborne@kyriba.com>
2024-05-13 11:58:44 +0200

    add comment

--- src/test/java/issue/org/axonframework/test/serviceconnectionissue/ServiceConnectionFailingTests.java
+++ src/test/java/issue/org/axonframework/test/serviceconnectionissue/ServiceConnectionFailingTests.java
@@ -20,7 +20,7 @@ import static org.hamcrest.Matchers.greaterThan;
 
 @Testcontainers
 @SpringBootTest
-@ActiveProfiles("axon")
+@ActiveProfiles("axon") // this profile overrides the AxonServer hostname but @ServiceConnection should have precendence
 class ServiceConnectionFailingTests {
 	@Autowired
 	private AxonServerConnectionManager axonServerConnectionManager;
